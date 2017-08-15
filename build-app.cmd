2b06b7a228a47ec6ae673900c175c0d4ee6d75f7
John.Riley <john.riley@bjss.com>
2017-08-15 15:28:52 -0400

    - Updated Build Scripts - Adds Android Platform to Project

--- build-app.cmd
+++ build-app.cmd
@@ -1,4 +1,6 @@
 cd testProject/
+echo "Adding Android Platform"
+call cordova platform add android
 echo "Updating PDF Renderer Plugin"
 call cordova plugin rm cordova-android-pdf-renderer-plugin
 call cordova plugin add ../cordova-android-pdf-renderer-plugin
--- build-plugin.cmd
+++ build-plugin.cmd
@@ -1,4 +1,6 @@
 cd cordova-android-pdf-renderer-plugin/
+echo "Adding Android Platform"
+call cordova platform add android
 echo "Running Cordova Build"
 call cordova build
 echo "Cordova Build Completed"
