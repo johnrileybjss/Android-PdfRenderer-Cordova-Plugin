1a58de601f6c7b1ac07adea89c1352e6d6c68d28
John.Riley <john.riley@bjss.com>
2017-08-18 13:27:41 -0400

    - Fixed Plugin Reference in tests.js

--- cordova-android-pdf-renderer-plugin/tests/tests.js
+++ cordova-android-pdf-renderer-plugin/tests/tests.js
@@ -1,198 +1,198 @@
 exports.defineAutoTests = function () {
-    describe('display (cordova.plugins.PdfRendererPlugin.display', function(){
+    describe('PdfRendererPlugin.display', function(){
         it('should be defined', function(){
-            expect(cordova.plugins.PdfRendererPlugin.display).toBeDefined();
+            expect(window.PdfRendererPlugin.display).toBeDefined();
         });
         it('should be a function', function(){
-            expect(typeof cordova.plugins.PdfRendererPlugin.display).toEqual("function");
+            expect(typeof window.PdfRendererPlugin.display).toEqual("function");
         });
         it("should throw an Error with no arguments", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.display();
+                window.PdfRendererPlugin.display();
             }).toThrowError("Filepath argument must be of type 'string'");
         });
         it("should throw an Error if the filepath argument is not a string", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.display(42, function() {}, function() {});
+                window.PdfRendererPlugin.display(42, function() {}, function() {});
             }).toThrowError("Filepath argument must be of type 'string'");
         });
         it("should throw an Error if the onSuccess callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.display("test", 42, function() {});
+                window.PdfRendererPlugin.display("test", 42, function() {});
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onFailure callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.display("test", function() {}, 42);
+                window.PdfRendererPlugin.display("test", function() {}, 42);
             }).toThrowError("onFailure callback must be of type 'function'");
         });
     });
 
-    describe('renderPage (cordova.plugins.PdfRendererPlugin.renderPage', function(){
+    describe('PdfRendererPlugin.renderPage', function(){
         it('should be defined', function(){
-            expect(cordova.plugins.PdfRendererPlugin.renderPage).toBeDefined();
+            expect(window.PdfRendererPlugin.renderPage).toBeDefined();
         });
         it('should be a function', function(){
-            expect(typeof cordova.plugins.PdfRendererPlugin.renderPage).toEqual("function");
+            expect(typeof window.PdfRendererPlugin.renderPage).toEqual("function");
         });
         it("should throw an Error with no arguments", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.renderPage();
+                window.PdfRendererPlugin.renderPage();
             }).toThrowError("Page Number argument must be of type 'number'");
         });
         it("should throw an Error if the page number argument is not a number", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.renderPage("test", function() {}, function() {});
+                window.PdfRendererPlugin.renderPage("test", function() {}, function() {});
             }).toThrowError("Page Number argument must be of type 'number'");
         });
         it("should throw an Error if the onSuccess callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.renderPage(42, 42, function() {});
+                window.PdfRendererPlugin.renderPage(42, 42, function() {});
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onFailure callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.renderPage(42, function() {}, 42);
+                window.PdfRendererPlugin.renderPage(42, function() {}, 42);
             }).toThrowError("onFailure callback must be of type 'function'");
         });
     });
 
-    describe('renderNextPage (cordova.plugins.PdfRendererPlugin.renderNextPage', function(){
+    describe('PdfRendererPlugin.renderNextPage', function(){
         it('should be defined', function(){
-             expect(cordova.plugins.PdfRendererPlugin.renderNextPage).toBeDefined();
+             expect(window.PdfRendererPlugin.renderNextPage).toBeDefined();
         });
         it('should be a function', function(){
-            expect(typeof cordova.plugins.PdfRendererPlugin.renderNextPage).toEqual("function");
+            expect(typeof window.PdfRendererPlugin.renderNextPage).toEqual("function");
         });
         it("should throw an Error with no arguments", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.renderNextPage();
+                window.PdfRendererPlugin.renderNextPage();
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onSuccess callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.renderNextPage(42, function() {});
+                window.PdfRendererPlugin.renderNextPage(42, function() {});
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onFailure callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.renderNextPage(function() {}, 42);
+                window.PdfRendererPlugin.renderNextPage(function() {}, 42);
             }).toThrowError("onFailure callback must be of type 'function'");
         });
     });
 
-    describe('renderPreviousPage (cordova.plugins.PdfRendererPlugin.renderPreviousPage', function(){
+    describe('PdfRendererPlugin.renderPreviousPage', function(){
         it('should be defined', function(){
-            expect(cordova.plugins.PdfRendererPlugin.renderPreviousPage).toBeDefined();
+            expect(window.PdfRendererPlugin.renderPreviousPage).toBeDefined();
         });
         it('should be a function', function(){
-            expect(typeof cordova.plugins.PdfRendererPlugin.renderPreviousPage).toEqual("function");
+            expect(typeof window.PdfRendererPlugin.renderPreviousPage).toEqual("function");
         });
         it("should throw an Error with no arguments", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.renderPreviousPage();
+                window.PdfRendererPlugin.renderPreviousPage();
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onSuccess callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.renderPreviousPage(42, function() {});
+                window.PdfRendererPlugin.renderPreviousPage(42, function() {});
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onFailure callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.renderPreviousPage(function() {}, 42);
+                window.PdfRendererPlugin.renderPreviousPage(function() {}, 42);
             }).toThrowError("onFailure callback must be of type 'function'");
         });
     });
-    describe('close (cordova.plugins.PdfRendererPlugin.close', function(){
+    describe('PdfRendererPlugin.close', function(){
         it('should be defined', function(){
-            expect(cordova.plugins.PdfRendererPlugin.close).toBeDefined();
+            expect(window.PdfRendererPlugin.close).toBeDefined();
         });
         it('should be a function', function(){
-            expect(typeof cordova.plugins.PdfRendererPlugin.close).toEqual("function");
+            expect(typeof window.PdfRendererPlugin.close).toEqual("function");
         });
         it("should throw an Error with no arguments", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.close();
+                window.PdfRendererPlugin.close();
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onSuccess callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.close(42, function() {});
+                window.PdfRendererPlugin.close(42, function() {});
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onFailure callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.close(function() {}, 42);
+                window.PdfRendererPlugin.close(function() {}, 42);
             }).toThrowError("onFailure callback must be of type 'function'");
         });
     });
-    describe('getPageInfo (cordova.plugins.PdfRendererPlugin.getPageInfo', function(){
+    describe('PdfRendererPlugin.getPageInfo', function(){
         it('should be defined', function(){
-            expect(cordova.plugins.PdfRendererPlugin.getPageInfo).toBeDefined();
+            expect(window.PdfRendererPlugin.getPageInfo).toBeDefined();
         });
         it('should be a function', function(){
-            expect(typeof cordova.plugins.PdfRendererPlugin.getPageInfo).toEqual("function");
+            expect(typeof window.PdfRendererPlugin.getPageInfo).toEqual("function");
         });
         it("should throw an Error with no arguments", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.getPageInfo();
+                window.PdfRendererPlugin.getPageInfo();
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onSuccess callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.getPageInfo(42, function() {});
+                window.PdfRendererPlugin.getPageInfo(42, function() {});
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onFailure callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.getPageInfo(function() {}, 42);
+                window.PdfRendererPlugin.getPageInfo(function() {}, 42);
             }).toThrowError("onFailure callback must be of type 'function'");
         });
     });
-    describe('getCurrentPageNumber (cordova.plugins.PdfRendererPlugin.getCurrentPageNumber', function(){
+    describe('PdfRendererPlugin.getCurrentPageNumber', function(){
         it('should be defined', function(){
-            expect(cordova.plugins.PdfRendererPlugin.getCurrentPageNumber).toBeDefined();
+            expect(window.PdfRendererPlugin.getCurrentPageNumber).toBeDefined();
         });
         it('should be a function', function(){
-            expect(typeof cordova.plugins.PdfRendererPlugin.getCurrentPageNumber).toEqual("function");
+            expect(typeof window.PdfRendererPlugin.getCurrentPageNumber).toEqual("function");
         });
         it("should throw an Error with no arguments", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.getCurrentPageNumber();
+                window.PdfRendererPlugin.getCurrentPageNumber();
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onSuccess callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.getCurrentPageNumber(42, function() {});
+                window.PdfRendererPlugin.getCurrentPageNumber(42, function() {});
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onFailure callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.getCurrentPageNumber(function() {}, 42);
+                window.PdfRendererPlugin.getCurrentPageNumber(function() {}, 42);
             }).toThrowError("onFailure callback must be of type 'function'");
         });
     });
-    describe('getPageCount (cordova.plugins.PdfRendererPlugin.getPageCount', function(){
+    describe('PdfRendererPlugin.getPageCount', function(){
         it('should be defined', function(){
-            expect(cordova.plugins.PdfRendererPlugin.getPageCount).toBeDefined();
+            expect(window.PdfRendererPlugin.getPageCount).toBeDefined();
         });
         it('should be a function', function(){
-            expect(typeof cordova.plugins.PdfRendererPlugin.getPageCount).toEqual("function");
+            expect(typeof window.PdfRendererPlugin.getPageCount).toEqual("function");
         });
         it("should throw an Error with no arguments", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.getPageCount();
+                window.PdfRendererPlugin.getPageCount();
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onSuccess callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.getPageCount(42, function() {});
+                window.PdfRendererPlugin.getPageCount(42, function() {});
             }).toThrowError("onSuccess callback must be of type 'function'");
         });
         it("should throw an Error if the onFailure callback is not a function", function() {
             expect(function() {
-                cordova.plugins.PdfRendererPlugin.getPageCount(function() {}, 42);
+                window.PdfRendererPlugin.getPageCount(function() {}, 42);
             }).toThrowError("onFailure callback must be of type 'function'");
         });
     });
